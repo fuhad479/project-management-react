2640ae36649e2490dcf053c30b6ec0333f8ce50d
fuhad479 <fuhadhasan479@gmail.com>
2022-12-16 17:14:26 +0600

    added login credentials to login page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -34,36 +34,49 @@ export default function Login() {
             <p className="text-[24px] font-thin mb-[16px]">
                 Sign in to project management
             </p>
-            <form
-                className="w-[310px] flex flex-col border border-[hsl(210deg,18%,87%)] rounded-[6px] bg-[hsl(210,29%,97%)] p-[16px]"
-                onSubmit={handleSubmit}
-            >
-                <FormField
-                    label="Email address"
-                    type="email"
-                    name="email-address"
-                    id="email-address"
-                    value={email}
-                    onChangeHandler={(e) => setEmail(e.target.value)}
-                    className="appearance-none block w-full text-[14px] border border-1-[hsl(210deg,18%,87%)] rounded-[6px] py-[5px] px-[12px] mt-[4px] mb-[16px] focus:outline-none focus:border-[#0969da]"
-                />
-                <FormField
-                    label="Password"
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={password}
-                    onChangeHandler={(e) => setPassword(e.target.value)}
-                    className="appearance-none block w-full text-[14px] border border-1-[hsl(210deg,18%,87%)] rounded-[6px] py-[5px] px-[12px] mt-[4px] mb-[16px] focus:outline-none focus:border-[#0969da]"
-                />
-                <input
-                    type="submit"
-                    name="submit"
-                    id="submit"
-                    value={isLoading ? 'Signing in' : 'Sign in'}
-                    className="appearance-none text-[14px] bg-[#2b5276] text-white border border-1 rounded-[6px] py-[5px] px-[16px] cursor-pointer hover:bg-[hsl(209,45%,29%)]"
-                />
-            </form>
+            <div className="flex gap-[16px]">
+                <div className="border border-[hsl(210deg,18%,87%)] rounded-[6px] p-[16px]">
+                    <p className="text-[14px] font-medium">Emails</p>
+                    <p className="text-[14px]">emily@example.com</p>
+                    <p className="text-[14px]">alex@example.com</p>
+                    <p className="text-[14px]">alison@example.com</p>
+                    <p className="text-[14px] mb-[16px]">john@example.com</p>
+                    <p className="text-[14px] font-medium">Password</p>
+                    <p className="text-[14px] mb-[16px]">123456</p>
+                </div>
+                <div className="">
+                    <form
+                        className="w-[310px] flex flex-col border border-[hsl(210deg,18%,87%)] rounded-[6px] bg-[hsl(210,29%,97%)] p-[16px]"
+                        onSubmit={handleSubmit}
+                    >
+                        <FormField
+                            label="Email address"
+                            type="email"
+                            name="email-address"
+                            id="email-address"
+                            value={email}
+                            onChangeHandler={(e) => setEmail(e.target.value)}
+                            className="appearance-none block w-full text-[14px] border border-1-[hsl(210deg,18%,87%)] rounded-[6px] py-[5px] px-[12px] mt-[4px] mb-[16px] focus:outline-none focus:border-[#0969da]"
+                        />
+                        <FormField
+                            label="Password"
+                            type="password"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChangeHandler={(e) => setPassword(e.target.value)}
+                            className="appearance-none block w-full text-[14px] border border-1-[hsl(210deg,18%,87%)] rounded-[6px] py-[5px] px-[12px] mt-[4px] mb-[16px] focus:outline-none focus:border-[#0969da]"
+                        />
+                        <input
+                            type="submit"
+                            name="submit"
+                            id="submit"
+                            value={isLoading ? 'Signing in' : 'Sign in'}
+                            className="appearance-none text-[14px] bg-[#2b5276] text-white border border-1 rounded-[6px] py-[5px] px-[16px] cursor-pointer hover:bg-[hsl(209,45%,29%)]"
+                        />
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
